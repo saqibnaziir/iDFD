8bba549315a1327b01822f0d93615a10f1c02ffe
Saqib Nazir <37848312+saqibnaziir@users.noreply.github.com>
2023-04-11 12:42:13 +0300

    Add files via upload
    Dispaly all the images in the selected dir

---
+++ dirreadimages.m
@@ -0,0 +1,28 @@
+%  Author: Qiu,Zhouyan, University of Vigo
+%  E-mail: zhouyan.qiu@uvigo.es
+%  This MATLAB-Code was developed under MATLAB R2021a.
+%  -------------------------------------------------------------------------------------------------------------------------------------
+%% Mfile name: dirshowallimages.m
+%  Revision history:
+%  24/06/2021  QIU  Original code
+%  -------------------------------------------------------------------------------------------------------------------------------------
+%  Description: Dispaly all the images in the selected dir
+%  -------------------------------------------------------------------------------------------------------------------------------------
+clc % clear command window
+clear % clear workspace
+close all % clear all graphics
+
+my_folder = uigetdir('', 'Open image files');
+file_pattern = fullfile(my_folder, '*.png');
+depth_files = dir(file_pattern);
+length = length(depth_files);
+
+for k = 1:length
+    base_fileName = depth_files(k).name;
+    full_fileName = fullfile(my_folder, base_fileName);
+    fprintf(1, 'Now reading %s\n', full_fileName);
+    depth_array = imread(full_fileName);
+    image(depth_array,'CDataMapping','scaled');  % Display image.
+    colorbar;
+    drawnow; % Force display to update immediately.
+end
